9f5f5b7df7cd6377636481f627b74d25239f6006
Lays Rodrigues <lays.rodrigues@kde.org>
2019-08-24 19:04:05 -0300

    Add schedule component
    Signed-off-by: Lays Rodrigues <lays.rodrigues@kde.org>

---
+++ src/components/schedule.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-container>
+    <v-expansion-panels>
+      <v-expansion-panel v-for="(item , i) in items" :key="i">
+        <v-expansion-panel-header>{{item}}</v-expansion-panel-header>
+      </v-expansion-panel>
+    </v-expansion-panels>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      items: [
+        "06/09",
+        "07/09",
+        "08/09",
+        "09/09",
+        "10/09",
+        "11/09",
+        "12/09",
+        "13/09"
+      ]
+    };
+  }
+};
+</script>
