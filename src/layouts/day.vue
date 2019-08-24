cf9228ef85ed1b627e2285da481c29cd65600a32
Lays Rodrigues <lays.rodrigues@kde.org>
2019-08-24 20:18:43 -0300

    Add layout for schedule days
    Signed-off-by: Lays Rodrigues <lays.rodrigues@kde.org>

---
+++ src/layouts/day.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-list>
+    <v-list-item-group v-model="schedule">
+      <v-list-item v-for="(item, i) in schedule" :key="i">
+        <v-list-item-content>
+          <v-list-item-title>{{ item }}</v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+    </v-list-item-group>
+  </v-list>
+</template>
+<script>
+export default {
+  props: ["schedule"]
+};
+</script>
