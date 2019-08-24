0c717a8236a6eaaa7b02397b099be2a22d0b8411
Lays Rodrigues <lays.rodrigues@kde.org>
2019-08-24 20:19:01 -0300

    Use layout on schedule component
    Signed-off-by: Lays Rodrigues <lays.rodrigues@kde.org>

--- src/components/schedule.vue
+++ src/components/schedule.vue
@@ -2,25 +2,32 @@
   <v-container>
     <v-expansion-panels>
       <v-expansion-panel v-for="(item , i) in items" :key="i">
-        <v-expansion-panel-header>{{item}}</v-expansion-panel-header>
+        <v-expansion-panel-header>{{item.date}}</v-expansion-panel-header>
+        <v-expansion-panel-content>
+          <Day :schedule="item.schedule" />
+        </v-expansion-panel-content>
       </v-expansion-panel>
     </v-expansion-panels>
   </v-container>
 </template>
 
 <script>
+import Day from "../layouts/day.vue";
 export default {
+  components: {
+    Day
+  },
   data() {
     return {
       items: [
-        "06/09",
-        "07/09",
-        "08/09",
-        "09/09",
-        "10/09",
-        "11/09",
-        "12/09",
-        "13/09"
+        { date: "06/09", schedule: [] },
+        { date: "07/09", schedule: [] },
+        { date: "08/09", schedule: [] },
+        { date: "09/09", schedule: [] },
+        { date: "10/09", schedule: [] },
+        { date: "11/09", schedule: [] },
+        { date: "12/09", schedule: [] },
+        { date: "13/09", schedule: [] }
       ]
     };
   }
