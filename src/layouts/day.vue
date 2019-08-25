1b81b2fd5d3979719fbaadcd8c06d48e706b4ea0
Lays Rodrigues <lays.rodrigues@kde.org>
2019-08-25 20:09:21 -0300

    Build day schedule list
    Signed-off-by: Lays Rodrigues <lays.rodrigues@kde.org>

--- src/layouts/day.vue
+++ src/layouts/day.vue
@@ -1,9 +1,19 @@
 <template>
   <v-list>
-    <v-list-item-group v-model="schedule">
+    <v-list-item-group>
       <v-list-item v-for="(item, i) in schedule" :key="i">
         <v-list-item-content>
-          <v-list-item-title>{{ item }}</v-list-item-title>
+          <v-list-item-title class="font-weight-medium">{{ item.talk_name }}</v-list-item-title>
+          <v-list-item-subtitle>
+            <v-icon>person_pin</v-icon>
+            {{ item.name }}
+          </v-list-item-subtitle>
+          <v-list-item-subtitle>
+            <v-icon>access_time</v-icon>
+            {{ item.talk_time }} |
+            <v-icon>meeting_room</v-icon>
+            {{ item.talk_room }}
+          </v-list-item-subtitle>
         </v-list-item-content>
       </v-list-item>
     </v-list-item-group>
