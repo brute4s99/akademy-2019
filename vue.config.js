fe56fe80e960904c37478348b7be6291153d950b
Lays Rodrigues <lays.rodrigues@kde.org>
2019-08-26 17:14:02 -0300

    Use vue config to setup workbox
    Signed-off-by: Lays Rodrigues <lays.rodrigues@kde.org>

---
+++ vue.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    pwa: {
+        workboxPluginMode: 'GenerateSW',
+        workboxOptions: {
+            runtimeCaching: [{
+                urlPattern: new RegExp('https://conf.kde.org/en/Akademy2019/public/schedule.json'),
+                handler: 'staleWhileRevalidate'
+            }]
+        }
+    }
+}
