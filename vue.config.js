4d8d8d01093f2dd827f48726d053276be16c9ab6
Lays Rodrigues <lays.rodrigues@kde.org>
2019-08-26 23:13:01 -0300

    Add more pwa setup to vue config
    Signed-off-by: Lays Rodrigues <lays.rodrigues@kde.org>

--- vue.config.js
+++ vue.config.js
@@ -5,7 +5,10 @@ module.exports = {
             runtimeCaching: [{
                 urlPattern: new RegExp('https://conf.kde.org/en/Akademy2019/public/schedule.json'),
                 handler: 'staleWhileRevalidate'
-            }]
+            }],
+            skipWaiting: true,
+            clientsClaim: true,
         }
-    }
+    },
+    publicPath: "akademy-2019"
 }
