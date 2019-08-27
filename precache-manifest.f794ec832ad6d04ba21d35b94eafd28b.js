b4853103e7031456db2ed75d97aec5e4b6528efb
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-08-27 02:36:43 +0000

    Deploy lays147/akademy-2019 to github.com/lays147/akademy-2019.git:gh-pages

---
+++ precache-manifest.f794ec832ad6d04ba21d35b94eafd28b.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "a5abcdc7a434c0ea130f",
+    "url": "/akademy-2019/js/app.e728a8dd.js"
+  },
+  {
+    "revision": "b1b1b22aeb8ea25cec2f",
+    "url": "/akademy-2019/css/chunk-vendors.10323651.css"
+  },
+  {
+    "revision": "b1b1b22aeb8ea25cec2f",
+    "url": "/akademy-2019/js/chunk-vendors.59b49faf.js"
+  },
+  {
+    "revision": "1f30dc520fec5745710ca6986a345be4",
+    "url": "/akademy-2019/img/Milan_Cathedral_(Duomo_di_Milano)_evening.1f30dc52.jpg"
+  },
+  {
+    "revision": "96c476804d7a788cc1c05351b287ee41",
+    "url": "/akademy-2019/fonts/MaterialIcons-Regular.96c47680.eot"
+  },
+  {
+    "revision": "0509ab09c1b0d2200a4135803c91d6ce",
+    "url": "/akademy-2019/fonts/MaterialIcons-Regular.0509ab09.woff2"
+  },
+  {
+    "revision": "29b882f018fa6fe75fd338aaae6235b8",
+    "url": "/akademy-2019/fonts/MaterialIcons-Regular.29b882f0.woff"
+  },
+  {
+    "revision": "da4ea5cdfca6b3baab285741f5ccb59f",
+    "url": "/akademy-2019/fonts/MaterialIcons-Regular.da4ea5cd.ttf"
+  },
+  {
+    "revision": "d5583f50d849511511f78f5c414d1419",
+    "url": "/akademy-2019/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/akademy-2019/robots.txt"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/akademy-2019/precache-manifest.f794ec832ad6d04ba21d35b94eafd28b.js"
+);
+
+workbox.core.setCacheNameDetails({prefix: "akademy2019"});
+
+workbox.skipWaiting();
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerRoute(/https:\/\/conf.kde.org\/en\/Akademy2019\/public\/schedule.json/, workbox.strategies.staleWhileRevalidate(), 'GET');
